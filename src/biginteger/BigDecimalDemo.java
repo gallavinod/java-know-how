52ddedf7636bfe7688b3cc17a8f2a00e40159d28
gallavinod <gallavinod@gmail.com>
2016-10-16 19:03:16 -0700

    Submitted Big Decimal

--- src/biginteger/BigDecimalDemo.java
+++ src/biginteger/BigDecimalDemo.java
@@ -6,7 +6,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 
-public class Runner {
+public class BigDecimalDemo {
 	
 	/*
 	 * https://www.hackerrank.com/challenges/java-bigdecimal
